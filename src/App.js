6331f9ff64f0e2d39e97a8f10aa054fa0f2f2ec9
taiming <taiming@bridgewell.com>
2021-03-26 16:01:08 +0800

    Add ability to download as pdf

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const AppWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+	padding: 20px;
 `;
 
 const App = () => {
--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import html2canvas from 'html2canvas';
+import { jsPDF } from "jspdf";
+
+/** Ref:
+ * https://stackoverflow.com/questions/23104008/where-to-change-default-pdf-page-width-and-font-size-in-jspdf-debug-js
+*/
+
+const ButtonGroup = styled.div`
+	display: flex;
+	padding: 12px 0px;
+	& > *:not(:first-child) {
+		margin-left: 12px;
+	}
+`;
 
 const Button = styled.button`
-	margin: 8px 0px;
 	cursor: pointer;
 `;
 
@@ -13,8 +25,7 @@ const CanvasWrapper = styled.div`
 `;
 
 const Canvas = () => {
-	const handleClickDownload = () => {
-		console.log('download');
+	const handleDownloadAsPng = () => {
 		html2canvas(document.querySelector("#capture")).then(canvas => {
 			var link = document.createElement('a');
 			link.download = 'filename.png';
@@ -23,9 +34,21 @@ const Canvas = () => {
 		});
 	};
 
+	const handleDownloadAsPdf = () => {
+		console.log('download as pdf')
+		var doc = new jsPDF('p', 'mm', [842, 598]); // p => portrait, l => landscape
+		html2canvas(document.querySelector("#capture")).then(canvas => {
+			doc.addImage(canvas.toDataURL("image/png"), 'PNG', 0, 0, canvas.width, canvas.height);
+			doc.save('sample-file.pdf');
+		});
+	}
+
 	return (
 		<div>
-			<Button onClick={handleClickDownload}>Click to download</Button>
+			<ButtonGroup>
+				<Button onClick={handleDownloadAsPng}>Download as *.png</Button>
+				<Button onClick={handleDownloadAsPdf}>Download as *.pdf</Button>
+			</ButtonGroup>
 			<CanvasWrapper id="capture" style={{ padding: '10px', background: '#f5da55' }}>
 				<h4 style={{ color: '#000' }}>Hello world!</h4>
 			</CanvasWrapper>
