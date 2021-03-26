846ff556eef0a0500a59630162a1cbd1fa950f7b
taiming <taiming@bridgewell.com>
2021-03-26 16:16:11 +0800

    Add student name and course name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const AppWrapper = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	padding: 20px;
+	padding: 60px 0px;
 `;
 
 const App = () => {
--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import html2canvas from 'html2canvas';
 import { jsPDF } from "jspdf";
 
+import templatePath from 'assets/certificate-template.png';
+
 /** Ref:
  * https://stackoverflow.com/questions/23104008/where-to-change-default-pdf-page-width-and-font-size-in-jspdf-debug-js
 */
+
+const InputGroup = styled.div`
+	padding: 20px 0px;
+`;
 
 const ButtonGroup = styled.div`
 	display: flex;
@@ -22,9 +28,36 @@ const Button = styled.button`
 const CanvasWrapper = styled.div`
 	width: 595px;
 	height: 842px;
+	position: relative;
+
+	background: url(${props => props.imageUrl});
+	background-size: contain;
+	background-repeat: no-repeat;
+`;
+
+const StudentName = styled.div`
+	position: absolute;
+	top: 380px;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	font-size: 32px;
+	font-weight: 700;
+`;
+
+const CourseName = styled.div`
+	position: absolute;
+	top: 430px;
+	width: 100%;
+	display: flex;
+	justify-content: center;
+	font-size: 24px;
 `;
 
 const Canvas = () => {
+	const [studentName, setStudentName] = useState('剔趣斯');
+	const [courseName, setCourseName] = useState('我要成為開課王');
+
 	const handleDownloadAsPng = () => {
 		html2canvas(document.querySelector("#capture")).then(canvas => {
 			var link = document.createElement('a');
@@ -41,6 +74,16 @@ const Canvas = () => {
 			doc.addImage(canvas.toDataURL("image/png"), 'PNG', 0, 0, canvas.width, canvas.height);
 			doc.save('sample-file.pdf');
 		});
+	};
+
+	const handleChangeStudentName = (event) => {
+		const value = event.target.value;
+		setStudentName(value);
+	}
+
+	const handleChangeCourseName = (event) => {
+		const value = event.target.value;
+		setCourseName(value);
 	}
 
 	return (
@@ -49,8 +92,19 @@ const Canvas = () => {
 				<Button onClick={handleDownloadAsPng}>Download as *.png</Button>
 				<Button onClick={handleDownloadAsPdf}>Download as *.pdf</Button>
 			</ButtonGroup>
-			<CanvasWrapper id="capture" style={{ padding: '10px', background: '#f5da55' }}>
-				<h4 style={{ color: '#000' }}>Hello world!</h4>
+			<InputGroup>
+				<div>
+					<label htmlFor="">學生姓名：</label>
+					<input type="text" value={studentName} onChange={handleChangeStudentName} />
+				</div>
+				<div>
+					<label htmlFor="">課程名稱：</label>
+					<input type="text" value={courseName} onChange={handleChangeCourseName} />
+				</div>
+			</InputGroup>
+			<CanvasWrapper id="capture" imageUrl={templatePath}>
+				<StudentName>{studentName}</StudentName>
+				<CourseName>{courseName}</CourseName>
 			</CanvasWrapper>
 		</div>
 	);
